1caa4824c25ec7ad9a526e7d744680e64c702db8
YOUR NAME <YOUR EMAIL@SAMPLE.COM>
2022-05-16 17:02:35 +0530

    user model and dependencies

---
+++ Backend/models/userModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+
+  
+    name: {
+        type: String,
+        required: [true,"Please enter your name"]  
+    },
+
+    email: {
+        type: String,
+        required: [true,"Please enter your email"],
+        trim:true
+    },
+    password: {
+        type: String,
+        required: [true,"Please enter a password"]
+    },
+
+    avatar: {
+        type: String,
+        default:"https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png"
+    },
+
+    mobile: {
+        type: String,
+        required: [true,"Please enter your mobile number"]
+    },
+
+    user_role: {
+        type: String,
+        default:"Student"
+    },
+    specialization: {
+        type: String,
+        default:""
+    },
+    research_area: {
+        type: String,
+        default:""
+    },
+    reg_number: {
+        type: String,
+        required: [true,"Please enter your registration number"]
+    }
+
+},{
+    timestamps : true
+})
+
+ 
+module.exports = mongoose.model("Users", userSchema);
