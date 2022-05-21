69ebd81315f5bc8838881b241af5979b8ae8a7d7
Yasantha Mihiran <yasanthamax@gmail.com>
2022-05-21 21:20:53 +0530

    marking-form

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -4,11 +4,12 @@ import EvaluateTopic from "./components/EvlauateTopic";
 import AcceptTopic from "./components/AcceptTopic";
 import TopicList from "./components/TopicList";
 import StdTopicList from "./components/StdTopicList";
+import AddMarking from "./components/AddMarking";
 
 function App() {
   return (
     <div>
-      <EvaluateTopic/>
+      <AddMarking/>
     </div>
   );
 }
