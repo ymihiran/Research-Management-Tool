d9ff52ca7a6dd1ea2937a0dcd5a37ab356f0dba4
Yasantha Mihiran <yasanthamax@gmail.com>
2022-05-21 16:37:31 +0530

    t-list-table

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import SubmitTopic from "./components/SubmitTopic";
 import EvaluateTopic from "./components/EvlauateTopic";
 import AcceptTopic from "./components/AcceptTopic";
+import TopicList from "./components/TopicList";
 
 function App() {
   return (
     <div>
-      <AcceptTopic/>
+      <TopicList/>
     </div>
   );
 }
