f79975c61ffb9a685a81386b47a0840728dc4f4c
YOUR NAME <it20265892@my.sliit.lk>
2022-05-29 01:48:00 +0530

    index file fix

--- Frontend/index.jsx
+++ Frontend/index.jsx
@@ -1,17 +1,6 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import App from "./App.jsx";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 import '/node_modules/bootstrap/dist/css/bootstrap.css';
-import DataProvider from './redux/store.js'
 
-ReactDOM.render(
-    <React.StrictMode>
-        <DataProvider>
-         <App/>
-        </DataProvider>
-    </React.StrictMode>,
-    document.getElementById('app')
-  
-);
-
-
+createRoot(document.getElementById("app")).render(<App />);
