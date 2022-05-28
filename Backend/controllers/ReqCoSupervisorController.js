f7eb960805f5c98d66c23c32f9c72d7d6965396b
Sanjula-Dulshan <sdulshan10@gmail.com>
2022-05-29 00:53:28 +0530

    get co supervisor to the frontend

--- Backend/controllers/ReqCoSupervisorController.js
+++ Backend/controllers/ReqCoSupervisorController.js
@@ -1,3 +1,16 @@
-import CoSupervisor from "../models/CoSupervisor.js";
+import coSuperVisor from "../models/userModel.js";
 
-export const ReqCoSupervisor = async (req, res) => {};
+export const ReqCoSupervisor = async (req, res) => {
+  const field = req.params.field;
+  const role = req.params.role;
+
+  console.log(req.params);
+  coSuperVisor
+    .find({ research_area: field, user_role: role })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
--- Frontend/components/RequestCoSupervisor.jsx
+++ Frontend/components/RequestCoSupervisor.jsx
@@ -1,46 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useTheme } from "@mui/material/styles";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import { GroupOutlined } from "@mui/icons-material";
+import axios from "axios";
 
-// const ITEM_HEIGHT = 48;
-// const ITEM_PADDING_TOP = 8;
-// const MenuProps = {
-//   PaperProps: {
-//     style: {
-//       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-//       width: 250,
-//     },
-//   },
-// };
-
-// const names = [
-//   "Oliver Hansen",
-//   "Van Henry",
-//   "April Tucker",
-//   "Ralph Hubbard",
-//   "Omar Alexander",
-//   "Carlos Abbott",
-//   "Miriam Wagner",
-//   "Bradley Wilkerson",
-//   "Virginia Andrews",
-//   "Kelly Snyder",
-// ];
-
-// function getStyles(name, personName, theme) {
-//   return {
-//     fontWeight:
-//       personName.indexOf(name) === -1
-//         ? theme.typography.fontWeightRegular
-//         : theme.typography.fontWeightMedium,
-//   };
-// }
 export default function RequestCoSupervisor() {
   const theme = useTheme();
-  const [personName, setPersonName] = React.useState([]);
+
   const [groupID, setGroupID] = useState();
   const [topic, setTopic] = useState();
   const [researchField, setResearchField] = useState();
@@ -49,17 +13,18 @@ export default function RequestCoSupervisor() {
     setGroupID(localStorage.getItem("groupID"));
     setTopic(localStorage.getItem("rTopic"));
     setResearchField(localStorage.getItem("rField"));
+
+    axios
+      .get(
+        `http://localhost:8070/supervisor/co/${localStorage.getItem(
+          "rField"
+        )}/${"Co-Supervisor"}`
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
   }, []);
 
-  const handleChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setPersonName(
-      // On autofill we get a stringified value.
-      typeof value === "string" ? value.split(",") : value
-    );
-  };
   return (
     <div className="body_container">
       {/*left side column */}
@@ -104,39 +69,6 @@ export default function RequestCoSupervisor() {
               className="form-control"
               id="researchTopic"
             />
-            {/* <FormControl
-              sx={{ width: 300, height: 45 }}
-              className="form-control me-5 "
-            >
-              <Select
-                displayEmpty
-                value={personName}
-                onChange={handleChange}
-                input={<OutlinedInput />}
-                renderValue={(selected) => {
-                  if (selected.length === 0) {
-                    return <em>Your Research Field</em>;
-                  }
-
-                  return selected.join(", ");
-                }}
-                MenuProps={MenuProps}
-                inputProps={{ "aria-label": "Without label" }}
-              >
-                <MenuItem disabled value="">
-                  <em>Your Research Field</em>
-                </MenuItem>
-                {names.map((name) => (
-                  <MenuItem
-                    key={name}
-                    value={name}
-                    style={getStyles(name, personName, theme)}
-                  >
-                    {name}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl> */}
           </div>
           <button type="submit" className="btn btn-success ">
             Find Co-Supervisor
