60b87b2fb0f5694c4aea729ae5ad6abe27e2c078
Yasantha Mihiran <yasanthamax@gmail.com>
2022-05-31 17:03:31 +0530

    main layout

--- Frontend/components/Main.jsx
+++ Frontend/components/Main.jsx
@@ -1,6 +1,6 @@
 import "./CSS/topicsub.css";
 import "./CSS/btrap.css";
-import { MDBCardHeader, MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from 'mdb-react-ui-kit'
+import {MDBAccordion, MDBAccordionItem ,MDBIcon ,MDBInput ,MDBBtn, MDBContainer ,MDBFooter ,MDBCardOverlay,  MDBCardHeader, MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from 'mdb-react-ui-kit'
 
 import Featured from "./Featured";
 
@@ -77,6 +77,33 @@ export default function Main(){
           </MDBCard>
           </div>
 
+          <MDBAccordion flush initialActive={1}>
+      <MDBAccordionItem collapseId={1} headerTitle='Accordion Item #1'>
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+        moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+        Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+        aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </MDBAccordionItem>
+      <MDBAccordionItem collapseId={2} headerTitle='Accordion Item #2'>
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+        moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+        Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+        aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </MDBAccordionItem>
+      <MDBAccordionItem collapseId={3} headerTitle='Accordion Item #3'>
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
+        moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+        Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+        aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </MDBAccordionItem>
+    </MDBAccordion>
+          
           <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
             <MDBCol>
               <MDBCard>
@@ -145,6 +172,187 @@ export default function Main(){
           </MDBRow>
           </div>
 
+          
+          
+
+          <MDBFooter className='text-center' color='white' bgColor='dark'>
+      <MDBContainer className='p-4'>
+        <section className='mb-4'>
+          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+            <MDBIcon fab icon='facebook-f' />
+          </a>
+
+          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+            <MDBIcon fab icon='twitter' />
+          </a>
+
+          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+            <MDBIcon fab icon='google' />
+          </a>
+
+          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+            <MDBIcon fab icon='instagram' />
+          </a>
+
+          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+            <MDBIcon fab icon='linkedin-in' />
+          </a>
+
+          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+            <MDBIcon fab icon='github' />
+          </a>
+        </section>
+
+        <section className=''>
+          <form action=''>
+            <div className='row d-flex justify-content-center'>
+              <div className='col-auto'>
+                <p className='pt-2'>
+                  <strong>Sign up for our newsletter</strong>
+                </p>
+              </div>
+
+              <MDBCol md='5' start='12'>
+                <MDBInput contrast type='email' label='Email address' className='mb-4' />
+              </MDBCol>
+
+              <div className='col-auto'>
+                <MDBBtn outline color='light' type='submit' className='mb-4'>
+                  Subscribe
+                </MDBBtn>
+              </div>
+            </div>
+          </form>
+        </section>
+
+        <section className='mb-4'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum repellat quaerat
+            voluptatibus placeat nam, commodi optio pariatur est quia magnam eum harum corrupti dicta, aliquam
+            sequi voluptate quas.
+          </p>
+        </section>
+
+        <section className=''>
+          <MDBRow>
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Links</h5>
+
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol>
+
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Links</h5>
+
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol>
+
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Links</h5>
+
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol>
+
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Links</h5>
+
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol>
+          </MDBRow>
+        </section>
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2020 Copyright:
+        <a className='text-white' href='https://mdbootstrap.com/'>
+          MDBootstrap.com
+        </a>
+      </div>
+    </MDBFooter>
         </div>  
         <div style={{backgroundColor:"#84809F"}}></div>  
       </div>
