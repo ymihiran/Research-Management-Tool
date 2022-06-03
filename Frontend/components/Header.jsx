10e304393a72a4f35b21f150585ff570e1cc96cd
Sanjula-Dulshan <sdulshan10@gmail.com>
2022-06-03 17:18:31 +0530

    add navigation to header

--- Frontend/components/Header.jsx
+++ Frontend/components/Header.jsx
@@ -21,12 +21,12 @@ export default function Header() {
             <div className="collapse navbar-collapse" id="navbarExample01">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item active">
-                  <a className="nav-link" aria-current="page" href="#">
+                  <a className="nav-link" aria-current="page" href="/">
                     Home
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="/profile">
                     Profile
                   </a>
                 </li>
